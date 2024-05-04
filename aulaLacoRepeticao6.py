f1b7b807e934c3d1dd36cb797fd28f2fc926f614
Guilherme Farias <88591245+xxfarias@users.noreply.github.com>
2024-05-03 21:50:09 -0300

    Add files via upload

---
+++ aulaLacoRepeticao6.py
@@ -0,0 +1,9 @@
+# for i in range(10):
+#     print("O valor de i é atualmente: ",i)
+
+# for i in range(1, 6):
+#     print("O valor de i é atualmente: ",i)
+
+
+for i in range(2, 8, 3):
+    print("O valor de i é atualmente: ",i)
